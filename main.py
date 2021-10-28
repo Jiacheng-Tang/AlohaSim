adb2dc150e5b1fe8bd7e509e365da8312403f5a1
Johnny Tang <tang.481@osu.edu>
2021-10-28 13:46:33 -0400

    10/28

--- main.py
+++ main.py
@@ -12,16 +12,21 @@ def run_sim1(p, num_user):
     return outcome
 
 
-def run_simN(p, num_user, num_channel):
+def run_simN(p, num_user,etaC):
     """Slotted Aloha N-channel"""
     outcome = 0
+    num_active = sum(random.binomial(1, p, num_user))
+    if num_active <= etaC:
+        outcome = num_user - num_active
+    else:
+        outcome = num_user
     return outcome
 
 
-def RL_MonteCarloTabular(n_episode, T):
+def RL_MonteCarloTabular(n_episode, T, epsilon):
     """Define Variables"""
     S_space = np.arange(0, 10)
-    A_space = 1 / np.arange(1, 20)
+    A_space = 1 / np.arange(1, 15)
     num_S = len(S_space)
     num_A = len(A_space)
     policy = np.tile(np.repeat(1 / num_A, num_A), (num_S, 1))
@@ -34,16 +39,22 @@ def RL_MonteCarloTabular(n_episode, T):
         S = np.repeat(0, T)
         A = np.repeat(0.5, T)
         R = np.repeat(0, T)
-        S[0] = random.choice(S_space, p=np.repeat(1 / num_S, num_S))
+        # S[0] = random.choice(S_space, p=np.repeat(1 / num_S, num_S))
+        S[0] = random.choice(S_space, p=S_space/sum(S_space))
         A[0] = random.choice(A_space, p=np.repeat(1 / num_A, num_A))
-        R[0] = -run_sim1(A[0], S[0])
+        # R[0] = -run_sim1(A[0], S[0])
+        R[0] = -run_simN(A[0], S[0], 2)
 
         """Generate an episode following given policy"""
         for t in range(1, T):
             S[t] = -R[t - 1]
             iSt = np.where(S_space == S[t])[0].item()
-            A[t] = random.choice(A_space, p=policy[iSt])
-            R[t] = -run_sim1(A[t], S[t])
+            if random.uniform(0,1,1)>epsilon:
+                A[t] = random.choice(A_space, p=policy[iSt])
+            else:
+                A[t] = random.choice(A_space, p=np.repeat(1 / num_A, num_A))
+            # R[t] = -run_sim1(A[t], S[t])
+            R[t] = -run_simN(A[t], S[t], 2)
             if S[t] == 0:
                 break
 
@@ -66,10 +77,12 @@ def RL_MonteCarloTabular(n_episode, T):
             mask[ia_star] = True
             policy[i][mask] = 1 / len(ia_star[0])
             policy[i][~mask] = 0
+
+    print(Qn)
     return Q, policy
 
 
 if __name__ == "__main__":
-    Q, policy = RL_MonteCarloTabular(1000, 100)
-    print(Q)
+    Q, policy = RL_MonteCarloTabular(10000, 50, 0.3)
+    # print(Q)
     print(policy)
